be67a546416e41c6d51f1818815df225b05909e6
Curtis Collard <jccollard3@gmail.com>
2015-12-14 20:43:16 -0500

    remove all is now complte A course can be remove from the entire system now.

--- lib/removeCourse.php
+++ lib/removeCourse.php
@@ -4,15 +4,8 @@ echo '<a href="/" >Home</a> <h5>POST:</h5>';
 print_r($_POST);
 echo '</br>------------------------		</br>';
 
-//load file for editing
-$ProgramsXML = simplexml_load_file('../course_programs.xml') or die("error: can't load course_programs.xml");
-
-$xpath = '/*/program[@name="'.$_POST['program'].'"]/course[@id="'.$_POST['course'].'"]'; // path to delte in programs.xml
-echo $xpath;
-unset($ProgramsXML->xpath($xpath)[0]->{0});
-$ProgramsXML->asXML('../course_programs.xml');
-
 if ($_POST['removeAll']) { // section removes all the related data for the course being removed.
+	$ProgramsXML = simplexml_load_file('../course_programs.xml') or die("error: can't load course_programs.xml");
 	$AntireqXML = simplexml_load_file("../course_antirequisite.xml") or die("error: can't load antireq xml");
 	$PrereqXML = simplexml_load_file("../course_prerequisite.xml") or die("error: can't load prereq xml");
 	$TermsXML = simplexml_load_file("../courseterms.xml") or die("error: can't load terms xml");
@@ -27,6 +20,21 @@ if ($_POST['removeAll']) { // section removes all the related data for the cours
 	unset($TermsXML->xpath($reqxpath)[0]->{0});
 	$TermsXML->asXML("../courseterms.xml");
 
+	// Remove course from all programs
+	$programxpath = '/*/*/course[@id="'.$_POST['course'].'"]';
+	echo $programxpath."</br>";
+	$toRemoveList = $ProgramsXML->xpath($programxpath);
+	foreach ($toRemoveList as $remove) {
+		unset($remove[0]->{0});
+		echo "</br>";
+	}
+	$ProgramsXML->asXML('../course_programs.xml');
+
+
+} else { // just remove it individually from the single program
+	$xpath = '/*/program[@name="'.$_POST['program'].'"]/course[@id="'.$_POST['course'].'"]'; // path to delte in programs.xml
+	unset($ProgramsXML->xpath($xpath)[0]->{0});
+	$ProgramsXML->asXML('../course_programs.xml');
 }
 
 
